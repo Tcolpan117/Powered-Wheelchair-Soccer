564bbd02ff6344cf3736d463413336af2c29c390
Logan Berk <146039828+LoganOBerk@users.noreply.github.com>
2026-09-18 12:47:02 -0400

    Interpolated the ball for smooth feeling when kicking and fixed the goal triggerbox so that the ball always returns to center on goal

--- Assets/Scripts/Governer.cs
+++ Assets/Scripts/Governer.cs
@@ -28,12 +28,13 @@ public class Governer : MonoBehaviour
 
     private void ScoreGoal(GameObject ball)
     {
+        Debug.Log($"ScoreGoal on {ball.name}");
         Rigidbody rb = ball.GetComponent<Rigidbody>();
 
         rb.linearVelocity = Vector3.zero; 
         rb.angularVelocity = Vector3.zero;
 
-        ball.transform.localPosition = Vector3.zero;
+        ball.transform.localPosition = Vector3.zero + Vector3.up;
     }
 
 }
--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -6,7 +6,7 @@ public class Trigger : MonoBehaviour
     public static event Action<GameObject, GameObject> TriggerEvent;
 
     private void OnTriggerEnter(Collider other)
-    {
+    {   Debug.Log($"Trigger hit: {name} by {other.name}");
         TriggerEvent?.Invoke(this.gameObject, other.gameObject);
     }
 
